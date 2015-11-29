454e1fd486d76c2d821f821f309cae7d0513805a
Carlos Sánchez Pérez <carloscsanchezperez@gmail.com>
2015-11-29 20:55:04 +0100

    update version ruby

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141229133347) do
+ActiveRecord::Schema.define(version: 20141229122211) do
 
   create_table "entities", force: :cascade do |t|
-    t.string   "name"
-    t.string   "address"
+    t.string   "name",       null: false
+    t.string   "address",    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -25,11 +25,13 @@ ActiveRecord::Schema.define(version: 20141229133347) do
     t.integer  "notice_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
   end
 
+  add_index "entity_roles", ["entity_id"], name: "index_entity_roles_on_entity_id"
+  add_index "entity_roles", ["notice_id"], name: "index_entity_roles_on_notice_id"
+
   create_table "notices", force: :cascade do |t|
-    t.string   "title"
+    t.string   "title",      null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
